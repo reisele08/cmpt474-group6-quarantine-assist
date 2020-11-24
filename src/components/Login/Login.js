f6acde4f4fe3fa1fddb46e25450bfebe1c290e65
reisele <richard_eisele@sfu.ca>
2020-11-24 11:33:38 -0800

    tasks default to 'open' status, volunteer button creates post request changing status to 'help offered'

---
+++ src/components/Login/Login.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Amplify from 'aws-amplify';
+import awsmobile from '../aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+import "./login.css"
+
+// richardtest
+// Adminpassword123~
+
+Amplify.configure(awsmobile);
+class Login extends Component {
+  render() {
+    return (
+      <div className="Login container">
+        <header className="Login-header">
+          <h1>Logged in</h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default withAuthenticator(Login, false);
+// export default Login;
